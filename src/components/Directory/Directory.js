7ae0b28471264d488171b91b9c2dfff7a3645ef5
Lokesh Debnath <lokeshdebnath8@gmail.com>
2022-08-23 11:09:35 +0530

    initial commit

---
+++ src/components/Directory/Directory.js
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ShopMen from "../../assets/shopMens.jpg";
+import ShopWomen from "../../assets/shopWomens.jpg";
+import "./Directory.scss";
+
+const Directory = () => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div className="item" style={{ backgroundImage: `url(${ShopWomen})` }}>
+          <a href="#">Shop Womens</a>
+        </div>
+
+        <div className="item" style={{ backgroundImage: `url(${ShopMen})` }}>
+          <a href="#">Shop Mens</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
